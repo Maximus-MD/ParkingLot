57f296a7d6e6b8e68bf471dbb06b3720c635dd20
Maximilian Stati <Maximilian.Stati@endava.com>
2024-11-05 14:40:22 +0200

    Initial Commit

---
+++ src/test/java/com/endava/md/internship/parkinglot/ParkinglotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.md.internship.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
